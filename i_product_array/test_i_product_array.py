76f3e0814ce9cee0e558e6f3cfb7d27f2568b7e4
Dino Macchi <Macchi_Manuel@ne.bah.com>
2021-07-03 16:02:41 -0400

    implemented product of other elements of the array

---
+++ i_product_array/test_i_product_array.py
@@ -0,0 +1,32 @@
+import unittest, random
+from i_product_array.i_product_array import prod_array
+
+class Params:
+    def __init__(self, input, output, res):
+        self.input = input
+        self.output = output
+
+
+class TestProductArray(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        random.seed(49)
+        return super().setUpClass()
+
+
+    def setUp(self):
+        self.testCases = {
+            'Definition Sample' : Params([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]),
+            'Empty list' : Params([], []),
+            'Other Example' : Params([3,2,1], [2,3,6]),
+        }
+
+
+    def test_multiple(self):
+        for desc, params in self.testCases.items():
+            with self.subTest(desc=desc):
+                self.assertEqual(prod_array(params.input), params.output) 
+
+if __name__ == '__main__':
+    unittest.main()
